98c39f91cf89cc695b6e6b41604b579325791448
lalitadogra <164787900+lalitadogra@users.noreply.github.com>
2024-03-27 23:46:52 +0530

    Update news.js

--- news.js
+++ news.js
@@ -15,14 +15,15 @@ const printData = (data) => {
 
 const onLoad = async (inpt) => {
    let res = await fetch(`https://newsapi.org/v2/everything?q=${inpt}&apiKey=${key}`, {
-  method: "GET", // or 'POST' if required
-  mode: "cors",
-  headers: {
-    "Content-Type": "application/json",
-    // Add any other headers if required
-  },
-domains: 'https://lalita-news.netlify.app/',
-       
+      method: "GET", // or 'POST' if required
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        // Add any other headers if required
+      },
+       everything:{
+           domains: 'https://lalita-news.netlify.app/',
+       }
     });
 
     let jsondata = await res.json();
