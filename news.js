f7536bc7b0562d8b9598ad04ca0da42da8a3ba34
lalitadogra <lalitadogra1612@gmail.com>
2024-05-23 14:17:09 +0530

    changing API

--- news.js
+++ news.js
@@ -1,12 +1,13 @@
-let key = "fb4364da62124c0f85552a25e7adedc0";
+let key = "d079c18ca14bca2db5403997036a1a1f";
 let newsgallery = document.getElementById("newsgallery")
-
+apikey = 'API_KEY';
+url = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=' + apikey;
 
 const printData = (data) => {
     newsgallery.innerHTML = ""
     data.forEach((elm) => newsgallery.innerHTML += `
         <div class="news-card">
-            <img src="${elm.urlToImage}">
+            <img src="${elm.image}">
             <h2>${elm.title}</h2>
             <p>${elm.description}</p>
         </div>
@@ -14,19 +15,27 @@ const printData = (data) => {
 }
 
 const onLoad = async (inpt) => {
-    let res = await fetch(`https://newsapi.org/v2/everything?q=${inpt}&apiKey=${key}`);
+    let res = await fetch(`https://gnews.io/api/v4/search?q=${inpt}&lang=en&max=10&apikey=${key}`);
     let jsondata = await res.json();
 
-    let articles = jsondata.articles
-    console.log(articles)
-
-    if(articles.length != 0 ){
-        printData(articles)
+    console.log(jsondata);
+    if(jsondata){
+            let articles = jsondata.articles
+        console.log(articles)
+    
+        if(articles.length != 0 ){
+            printData(articles)
+        }else{
+            newsgallery.innerHTML = ""
+    
+            newsgallery.innerHTML =  "<h1>Something went wrong</h1>"
+        }
     }else{
-        newsgallery.innerHTML = ""
-
-        newsgallery.innerHTML =  "<h1>Something went wrong</h1>"
+         newsgallery.innerHTML = ""
+         newsgallery.innerHTML =  `<h1>${jsondata.message}</h1>`
     }
+    
+
 }
 
 onLoad("india")
